e789bacd6f0919613e2024ff7928ea212e7cbc56
SENGHAK.CHHUN <SENGHAK.CHHUN@PNC.LAN>
2023-12-14 09:44:11 +0700

    #4 update add category feature

--- js/category.js
+++ js/category.js
@@ -13,7 +13,6 @@ const backBtn = document.querySelector("#back");
 backBtn.addEventListener("click", backCategory);
 search.addEventListener("input", searchCategory);
 let i = 0;
-let run = false;
 let NumberIDcategory = 0;
 function clearForm() {
   nameCategory.value = "";
@@ -38,11 +37,7 @@ function loadLocalCategory() {
 }
 function searchCategory() {
   for (let searchCat of tbody.children) {
-    if (
-      searchCat.children[1].textContent
-        .toLowerCase()
-        .includes(search.value.toLowerCase())
-    ) {
+    if (searchCat.children[1].textContent.toLowerCase().includes(search.value.toLowerCase())) {
       searchCat.style.display = "";
     } else {
       searchCat.style.display = "none";
@@ -50,57 +45,82 @@ function searchCategory() {
   }
 }
 function deleteCategory(e) {
-  console.log(data.category)
-  console.log(e.target.closest('tr').children);
+  let listNameOfCategory = data.category;
+  let nameOfCategory = e.target.closest('tr');
+  for (let name of listNameOfCategory) {
+    if (name.category == nameOfCategory.children[1].textContent) {
+      nameOfCategory.remove()
+
+    }
+  }
 }
 function addCategory() {
-  table.style.display = "none";
   form.style.display = "";
 }
 function saveCategory(event) {
-  table.style.display = "";
-  form.style.display = "none";
-  NumberIDcategory += 1;
   event.preventDefault();
-  let obj = {};
-  obj.id = NumberIDcategory;
-  obj.category = nameCategory.value;
-  obj.description = descirptionCategory.value;
-  data.category.push(obj);
-  saveLocalCategory();
-  clearForm();
-  let row = document.createElement("tr");
-  let tdID = document.createElement("td");
-  let tdName = document.createElement("td");
-  tdID.textContent = obj.id;
-  tdName.textContent = obj.category;
-  let tdAction = document.createElement("td");
-  tdAction.className = "action";
-  let delButton = document.createElement("button");
-  let viewButton = document.createElement("button");
-  let icondel = document.createElement("i");
-  let spandel = document.createElement("span");
-  let iconview = document.createElement("i");
-  let spanview = document.createElement("span");
-  viewButton.setAttribute("id", "detail");
-  delButton.setAttribute("id", "delete");
-  delButton.addEventListener("click", deleteCategory);
-  icondel.className = "material-icons icons-color";
-  iconview.className = "material-icons icons-color";
-  icondel.textContent = "delete";
-  spandel.textContent = "Delete";
-  iconview.textContent = "create";
-  spanview.textContent = "Detail";
-  delButton.appendChild(icondel);
-  delButton.appendChild(spandel);
-  viewButton.appendChild(iconview);
-  viewButton.appendChild(spanview);
-  tdAction.appendChild(delButton);
-  tdAction.appendChild(viewButton);
-  row.appendChild(tdID);
-  row.appendChild(tdName);
-  row.appendChild(tdAction);
-  tbody.appendChild(row);
+  form.style.display = "none";
+  let run = true;
+  let i = 0;
+  let listNameOfCategory = data.category;
+  for (let name of listNameOfCategory) {
+    if (nameCategory.value == name.category) {
+      i += 1
+    }
+  }
+  if (i > 0) {
+    run = false;
+  }
+  if (run) {
+    let storeId = data.categoryID;
+    if (storeId == null) {
+      storeId = 1;
+      data.categoryID = storeId
+    } else {
+      storeId += 1
+      data.categoryID = storeId
+    }
+    let obj = {};
+    obj.id = data.categoryID;
+    obj.category = nameCategory.value;
+    obj.description = descirptionCategory.value;
+    data.category.push(obj);
+    saveLocalCategory();
+    let row = document.createElement("tr");
+    let tdID = document.createElement("td");
+    let tdName = document.createElement("td");
+    tdID.textContent = obj.id;
+    tdName.textContent = obj.category;
+    let tdAction = document.createElement("td");
+    tdAction.className = "action";
+    let delButton = document.createElement("button");
+    let viewButton = document.createElement("button");
+    let icondel = document.createElement("i");
+    let spandel = document.createElement("span");
+    let iconview = document.createElement("i");
+    let spanview = document.createElement("span");
+    viewButton.setAttribute("id", "detail");
+    delButton.setAttribute("id", "delete");
+    delButton.addEventListener("click", deleteCategory);
+    icondel.className = "material-icons icons-color";
+    iconview.className = "material-icons icons-color";
+    icondel.textContent = "delete";
+    spandel.textContent = "Delete";
+    iconview.textContent = "create";
+    spanview.textContent = "Detail";
+    delButton.appendChild(icondel);
+    delButton.appendChild(spandel);
+    viewButton.appendChild(iconview);
+    viewButton.appendChild(spanview);
+    tdAction.appendChild(delButton);
+    tdAction.appendChild(viewButton);
+    row.appendChild(tdID);
+    row.appendChild(tdName);
+    row.appendChild(tdAction);
+    tbody.appendChild(row);
+    run = true;
+    clearForm();
+  }
 }
 function createTablerow() {
   let storeCategory = data.category;
@@ -137,7 +157,6 @@ function createTablerow() {
     row.appendChild(tdName);
     row.appendChild(tdAction);
     tbody.appendChild(row);
-    run = true;
   }
 }
 let data = {
